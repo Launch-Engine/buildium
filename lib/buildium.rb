d931c578060310e6cde20ddeb64c3a991575afaa
Wes Hays <weshays@gamil.com>
2021-06-11 18:40:14 -0700

    updated main file

--- lib/buildium.rb
+++ lib/buildium.rb
@@ -2,7 +2,19 @@
 
 require_relative "buildium/version"
 
-module Buildium
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative 'buildium/buildium'
+
+require_relative 'buildium/configuration'
+require_relative 'buildium/buildium_error'
+require_relative 'buildium/buildium_result'
+require_relative 'buildium/buildium_result_set'
+
+require_relative 'buildium/base'
+require_relative 'buildium/owner'
+require_relative 'buildium/rental_property'
+
+
+# module Buildium
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
