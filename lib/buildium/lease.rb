999344af0da8356f0301b7f2c09602f5f4072e11
Wes Hays <weshays@gamil.com>
2021-07-21 16:36:42 -0700

    Added support for rental leases

---
+++ lib/buildium/lease.rb
@@ -0,0 +1,7 @@
+module Buildium
+  class Lease < Buildium::Base
+    def self.path
+      'leases'
+    end
+  end
+end
--- test.rb
+++ test.rb
@@ -18,46 +18,10 @@ end
 
 # properties = Buildium::RentalProperty.list(limit: 5)
 # transactions = Buildium::GeneralLedgerTransaction.list(startdate: '2021-01-01', enddate: '2021-06-11', glaccountids: 11)
+# property_units = Buildium::RentalPropertyUnit.list(limit: 1000, propertyids: '9115,9116')
 
-
-#binding.pry
-property_units = Buildium::RentalPropertyUnit.list(limit: 1000, propertyids: '9115,9116')
+leases = Buildium::Lease.list(limit: 5, propertyids: '9115')
 
 binding.pry
 
-adf=3
-
-# @data=
-# [{:id=>41071939,
-#   :date=>"2021-01-11",
-#   :transaction_type=>"ElectronicFundsTransfer",
-#   :total_amount=>619.25,
-#   :check_number=>"",
-#   :unit_agreement=>nil,
-#   :unit_id=>nil,
-#   :unit_number=>nil,
-#   :payment_detail=>
-#    {:payment_method=>"Check",
-#     :payee=>{:id=>749239, :name=>"2716 Saint Edwards Circle LLC", :type=>"RentalOwner", :href=>"https://api.buildium.com/v1/rentals/owners/749239"},
-#     :is_internal_transaction=>true,
-#     :internal_transaction_status=>{:is_pending=>false, :result_date=>"2021-01-11", :result_code=>"S01"}},
-#   :journal=>
-#    {:memo=>"Owner Draw",
-#     :lines=>
-#      [{:gl_account=>
-#         {:id=>11,
-#          :name=>"Owner Draw",
-#          :description=>"Owner Draw",
-#          :type=>"Equity",
-#          :sub_type=>"Equity",
-#          :is_default_gl_account=>true,
-#          :is_contra_account=>true,
-#          :is_bank_account=>false,
-#          :cash_flow_classification=>"FinancingActivities",
-#          :exclude_from_cash_balances=>false,
-#          :sub_accounts=>nil},
-#        :amount=>619.25,
-#        :is_cash_posting=>true,
-#        :reference_number=>"",
-#        :memo=>"Owner Draw",
-#        :accounting_entity=>{:id=>123286, :accounting_entity_type=>"Rental", :href=>"https://api.buildium.com/v1/rentals/123286"}}]}},
+asdf=3
