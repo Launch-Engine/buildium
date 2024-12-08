8dee84961cd38bad486dde209b46e14cf506d5cb
Wes Hays <weshays@gmail.com>
2024-12-08 09:39:43 -0800

    Added error  check for BuildiumError

--- lib/buildium/buildium_error.rb
+++ lib/buildium/buildium_error.rb
@@ -6,11 +6,34 @@ module Buildium
     attr_reader :response_code
 
     def initialize(http_response)
-      data = JSON.parse(http_response.body, symbolize_names: true).to_snake_keys
+      data = parse_response(http_response)
       @error = data[:user_message]
       @errors = data[:errors]
       @error_message = data[:error_code]
       @response_code = http_response.response_code
+    end
+
+    def default_response(response)
+      {
+        user_message: 'Unknown response',
+        errors: ['Unknown response'],
+        error_code: response.response_code
+      }
+    end
+
+    def not_found_response(response)
+      {
+        user_message: 'Resource not found',
+        errors: ['Resource not found'],
+        error_code: response.response_code
+      }
+    end
+
+    def parse_response(response)
+      return JSON.parse(response.body, symbolize_names: true).to_snake_keys unless response.body.to_s == ''
+      return not_found_response(response) if response.response_code == 404
+
+      default_response(response)
     end
   end
 end
